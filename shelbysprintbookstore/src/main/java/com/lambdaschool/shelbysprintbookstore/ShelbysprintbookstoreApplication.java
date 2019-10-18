c4986ed4fe23d9d4fedb2be6e3f2d27a05db5bac
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-10-18 08:41:02 -0700

    initial commit, set up project on my own branch

---
+++ shelbysprintbookstore/src/main/java/com/lambdaschool/shelbysprintbookstore/ShelbysprintbookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.shelbysprintbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelbysprintbookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelbysprintbookstoreApplication.class, args);
+    }
+
+}
