6a2de443cc7aaad37c0c02bb9a01806ee579af7d
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-10-20 13:35:11 -0700

    updated dependencies and deployed!

--- shelbysprintbookstore/src/main/java/com/lambdaschool/shelbysprintbookstore/ShelbysprintbookstoreApplication.java
+++ shelbysprintbookstore/src/main/java/com/lambdaschool/shelbysprintbookstore/ShelbysprintbookstoreApplication.java
@@ -1,13 +1,34 @@
 package com.lambdaschool.shelbysprintbookstore;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+@EnableWebMvc
+@EnableJpaAuditing
 @SpringBootApplication
-public class ShelbysprintbookstoreApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ShelbysprintbookstoreApplication.class, args);
+public class ShelbysprintbookstoreApplication
+{
+    private static boolean stop = false;
+    @Autowired
+    private static Environment env;
+    private static void checkEnvironmentVariable(String envvar)
+    {
     }
-
+    public static void main(String[] args)
+    {
+        checkEnvironmentVariable("OAUTHCLIENTID");
+        checkEnvironmentVariable("OAUTHCLIENTSECRET");
+        if (!stop)
+        {
+            ApplicationContext ctx = SpringApplication.run(ShelbysprintbookstoreApplication.class,
+                    args);
+            DispatcherServlet dispatcherServlet = (DispatcherServlet) ctx.getBean("dispatcherServlet");
+            dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+        }
+    }
 }
--- shelbysprintbookstore/src/main/java/com/lambdaschool/shelbysprintbookstore/config/AuthorizationServerConfig.java
+++ shelbysprintbookstore/src/main/java/com/lambdaschool/shelbysprintbookstore/config/AuthorizationServerConfig.java
@@ -21,13 +21,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     static final String GRANT_TYPE_PASSWORD = "password";
     static final String AUTHORIZATION_CODE = "authorization_code";
-    static final String REFRESH_TOKEN = "refresh_token";
     static final String IMPLICIT = "implicit";
     static final String SCOPE_READ = "read";
     static final String SCOPE_WRITE = "write";
     static final String TRUST = "trust";
     static final int ACCESS_TOKEN_VALIDITY_SECONDS = 1 * 60 * 60;
-    static final int FREFRESH_TOKEN_VALIDITY_SECONDS = 6 * 60 * 60;
 
     @Autowired
     private TokenStore tokenStore;
@@ -43,13 +41,24 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     {
         //                .authorizedGrantTypes(GRANT_TYPE_PASSWORD, AUTHORIZATION_CODE, REFRESH_TOKEN, IMPLICIT)
 
-        configurer.inMemory().withClient(CLIENT_ID).secret(encoder.encode(CLIENT_SECRET)).authorizedGrantTypes(GRANT_TYPE_PASSWORD, AUTHORIZATION_CODE, IMPLICIT).scopes(SCOPE_READ, SCOPE_WRITE, TRUST).accessTokenValiditySeconds(ACCESS_TOKEN_VALIDITY_SECONDS).refreshTokenValiditySeconds(FREFRESH_TOKEN_VALIDITY_SECONDS);
+        configurer.inMemory()
+                .withClient(CLIENT_ID)
+                .secret(encoder.encode(CLIENT_SECRET))
+                .authorizedGrantTypes(GRANT_TYPE_PASSWORD,
+                        AUTHORIZATION_CODE,
+                        IMPLICIT)
+                .scopes(SCOPE_READ,
+                        SCOPE_WRITE,
+                        TRUST)
+                .accessTokenValiditySeconds(ACCESS_TOKEN_VALIDITY_SECONDS);
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception
     {
-        endpoints.tokenStore(tokenStore).authenticationManager(authenticationManager);
-        endpoints.pathMapping("/oauth/token", "/login");
+        endpoints.tokenStore(tokenStore)
+                .authenticationManager(authenticationManager);
+        endpoints.pathMapping("/oauth/token",
+                "/login");
     }
 }
